052b08c89c4c96dd852038aec33870735b2399ef
niransingh <nsl0707@gmail.com>
2016-07-04 17:19:31 +0530

    first commit

---
+++ www/js/index.js
@@ -0,0 +1,78 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+        app.getContacts();
+    },
+     // Cordova is ready
+    //
+    getContacts:function() {
+        // find all contacts with 'Bob' in any name field
+        var options = new ContactFindOptions();
+        options.filter=""; 
+        var fields = ["*"];//["displayName", "name"];
+        navigator.contacts.find(fields, app.onSuccess, app.onError, options);
+    },
+
+    // onSuccess: Get a snapshot of the current contacts
+    //
+    onSuccess:function(contacts) {
+        document.querySelector(".app").style.display = "none";
+        document.querySelector(".contact-container").style.display = "block";
+        var contact_list = document.querySelector("#contact-list");
+        for (var i=0; i<contacts.length; i++) {
+            var li = document.createElement("li");
+            li.innerHTML = "<a href='#'>"+contacts[i].displayName+"</a>";
+            contact_list.appendChild(li);
+        }
+    },
+
+    // onError: Failed to get the contacts
+    //
+    onError:function(contactError) {
+        alert('onError!' + JSON.stringify(contactError));
+    }
+};
